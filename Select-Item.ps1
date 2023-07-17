8b2cf679101d5102dc60b7fa5d00ce4c3e656415
Olaf <52982343+BOfH-666@users.noreply.github.com>
2023-07-17 20:21:34 +0200

    Update Select-Item.ps1 flexible columns count
    depending on the amount of list items the number of columns is limited to 1 for a maximum of 25 items and to 2 for a maximum of 60 items

--- Select-Item.ps1
+++ Select-Item.ps1
@@ -1,141 +1,110 @@
-﻿
-function Select-Item {
+﻿function Select-Item {
     <#
         .SYNOPSIS
             Select an item from a given list.
-
         .DESCRIPTION
-            This function shows a vertically sorted list of unique items in three columns and lets you choose one of them.
-
+            This function shows a vertically sorted list of unique items in columns and lets you choose one of them.
         .PARAMETER  itemList
             Specifies the list of items to choose from
-
         .EXAMPLE
             PS C:\> Select-Item -itemList 'Peter', 'Paul', 'Mary', 'John', 'George', 'Paul', 'Ringo', 'Mick', 'Keith', 'Ron', 'Charlie'
-
             This command eliminates the double occurance of Paul and shows the rest of the names including an index number for each of them in three columns.
-
             1 - Peter             5 - George           8 - Keith
             2 - Paul              6 - Ringo            9 - Ron
             3 - Mary              7 - Mick            10 - Charlie
             4 - John
-
             Please enter index of the desired item (<Enter> = 0 = cancel):  
-
         .INPUTS
             System.Array
-
         .OUTPUTS
             System.String
-
         .NOTES
             The built in cmdlet Format-Wide provides a way of formatting any given list in columns but the order would be horizontal, which I didn't like. That's why I created this function
-
         .LINK
             about_functions_advanced
-
         .LINK
             about_comment_based_help
-
     #>
     [CmdletBinding()]
     [OutputType([System.String])]
     param(
         [Parameter(Position = 0, Mandatory = $true)]
         [ValidateNotNullOrEmpty()]
-        [System.Array]
+        [System.Collections.Generic.List[String]]
         $itemList
     )
-    $itemList = $itemList | Select-Object -Unique
-    $displayList = foreach ($item in $itemList) {
-        [PSCustomObject]@{
-            Name        = $item
-            Index       = $itemList.IndexOf($item) + 1
-            DisplayName = "{0,3} - {1}" -f $($itemList.IndexOf($item) + 1), $item
+    begin {
+        if ($itemList.Length -eq 1) {
+            Return $itemList[0]
         }
-    }
-
-    if ($itemList.Length -gt 3) {
-        $FloorThird = [Math]::Floor($itemList.Count / 3)
-        $Modulo = $itemList.Count - ($FloorThird * 3 )
-
-
-        switch ($Modulo) {
-            1 {
-                $Chunk1 = $displayList | Select-Object -First ( $FloorThird + 1 )
-                $Chunk2 = $displayList | Select-Object -Skip  ( $FloorThird + 1 ) -First $FloorThird
-                $Chunk3 = $displayList | Select-Object -Last  $FloorThird
-            }
-            2 {
-                $Chunk1 = $displayList | Select-Object -First ( $FloorThird + 1 )
-                $Chunk2 = $displayList | Select-Object -Skip  ( $FloorThird + 1 ) -First ( $FloorThird + 1 )
-                $Chunk3 = $displayList | Select-Object -Last  $FloorThird
-            }
-            Default { 
-                $Chunk1 = $displayList | Select-Object -First $FloorThird
-                $Chunk2 = $displayList | Select-Object -Skip  $FloorThird -First $FloorThird
-                $Chunk3 = $displayList | Select-Object -Last  $FloorThird
-            }
+        if ($itemList.Count -ne ($itemList | Select-Object -Unique).count) {
+            Write-Error "the provided list contains duplicate elements. '$($itemList)'"
+            break
         }
 
-        $DisplayIndexStartChunk1 = -2
-        $ColumnList = $Chunk1 | 
-        ForEach-Object {
+        $displayList = foreach ($item in $itemList) {
             [PSCustomObject]@{
-                Data         = $_.Name
-                Index        = $_.Index
-                DisplayName  = $_.DisplayName
-                DisplayIndex = $DisplayIndexStartChunk1 += 3
+                Name        = $item
+                Index       = $itemList.IndexOf($item) + 1
+                DisplayName = "{0,3} - {1}" -f $($itemList.IndexOf($item) + 1), $item
             }
         }
-        $DisplayIndexStartChunk2 = -1
-        $ColumnList += $Chunk2 | 
-        ForEach-Object {
-            [PSCustomObject]@{
-                Data         = $_.Name
-                Index        = $_.Index
-                DisplayName  = $_.DisplayName
-                DisplayIndex = $DisplayIndexStartChunk2 += 3
+        $LongestElement = 
+        ($displayList | Sort-Object -Property { $_.DisplayName.toString().Length } | Select-Object -Last 1 -ExpandProperty DisplayName).length
+        $ConsoleWidth = $Host.UI.RawUI.WindowSize.Width
+        $CalculatedColumnCount = [Math]::Floor( $ConsoleWidth / ($LongestElement + 10))
+    }
+    process {
+        if ($displayList.count -le 25) {
+            $MaxColumnCount = 1
+        }
+        elseif ($displayList.count -le 60) {
+            $MaxColumnCount = 2
+        }
+        if ($MaxColumnCount) {
+            $DisplayColumnCount = $MaxColumnCount
+        }
+        else {
+            $DisplayColumnCount = $CalculatedColumnCount
+        }
+        $DisplayItemList = [System.Collections.Generic.List[String]]$($displayList.DisplayName)
+        $Rows = [Math]::Ceiling($displayList.count / $DisplayColumnCount)
+        $Matrix = New-Object 'object[,]' $DisplayColumnCount, $Rows
+        foreach ($Column in @(0..($DisplayColumnCount - 1))) {
+            foreach ($Row in @(0..($Rows - 1))) {
+                if ($DisplayItemList.Count -gt 0) {
+                    $Matrix[$Column, $Row] = $DisplayItemList.Item(0).toString()
+                    $DisplayItemList.RemoveAt(0)
+                }
             }
         }
-        $DisplayIndexStartChunk3 = 0
-        $ColumnList += $Chunk3 | 
-        ForEach-Object {
-            [PSCustomObject]@{
-                Data         = $_.Name
-                Index        = $_.Index
-                DisplayName  = $_.DisplayName
-                DisplayIndex = $DisplayIndexStartChunk3 += 3
+        $ColumnList =
+        foreach ($Row in @(0..($Rows - 1))) {
+            $CompleteRow =
+            foreach ($Column in @(0..($DisplayColumnCount - 1))) {
+                "{0,-$($LongestElement + 6)}" -f $Matrix[$Column, $Row]
             }
+            $CompleteRow -join ' '
         }
+        $ColumnList | Out-Host
     }
-    elseif ($itemList.Length -eq 1) {
-        return $itemList
-    }
-    else {
-        $ColumnList = $displayList
-    }
-    $ColumnList | Sort-Object -Property DisplayIndex | Format-Wide -Column 3 -Property DisplayName | Out-Host
-
-    [int]$choice = 0
-    do {
-        try {
-            [int]$choice = Read-Host 'Please enter index of the desired item (<Enter> = 0 = cancel)'  -ErrorAction Stop
-            Write-Verbose "Select-Item `$choice: $choice"
+    end {
+        [int]$choice = 0
+        do {
+            try {
+                [int]$choice = Read-Host "`nPlease enter index of the desired item (<Enter> = 0 = cancel)"  -ErrorAction Stop
+            }
+            catch {
+                Write-Warning "Incorrect input! Please choose a numeric value between '0' and '$($displayList.Length )'!"
+            } 
         }
-        catch {
-            Write-Warning "Incorrect input! Please choose a numeric value between '0' and '$($itemList.Length )'!"
-        } 
+        until ($choice -ge 0 -and $choice -le $displayList.Length )
+        if ($choice -eq 0) {
+            $selectedItem = 'none'
+        }
+        else {
+            $selectedItem = $displayList[$choice - 1].Name
+        }
+        return $selectedItem
     }
-    until ($choice -ge 0 -and $choice -lt $itemList.Length + 1 )
-    if ($choice -eq 0) {
-        $selectedItem = 'none'
-    }
-    else {
-        $selectedItem = $itemList[$choice - 1 ] 
-    }
-    Write-Verbose "Select-Item `$selectedItem: $selectedItem"
-
-    return $selectedItem
 }
-
